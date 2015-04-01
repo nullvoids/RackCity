8e3370a670ae564ef12026e3c763cff98b85bddc
sollabeck <kevin.ceballos@gmail.com>
2015-03-31 22:23:25 -0700

    finish mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 require 'httparty'
-
+require 'geocoder'
 get '/' do
   erb :index
 end
@@ -7,12 +7,18 @@ end
 get '/map' do
   erb :map
 end
-
+#long is neg
 post '/map' do
   @start_end={starting: params[:start],
    ending: params[:end]
     }.to_json
-  @data=HTTParty.get('https://data.sfgov.org/resource/w969-5mn4.json?$where=within_circle(latitude,37.78346622,-122.42177834,300)')
+  # @location_coordinates= {
+  #   start: {longitude: Geocoder.search(params[:start])[0].longitude, latitude:Geocoder.search(params[:start])[0].latitude},
+  #   destination: {longitude: Geocoder.search(params[:end])[0].longitude, latitude:Geocoder.search(params[:end])[0].latitude}}.to_json
+  latitude=Geocoder.search(params[:end])[0].latitude
+  longitude=Geocoder.search(params[:end])[0].longitude
+  @coordinates= {longitude: longitude, latitude:latitude}.to_json
+  @data=HTTParty.get('https://data.sfgov.org/resource/w969-5mn4.json?$where=within_circle(latitude,'+latitude.to_s+','+longitude.to_s+',200)')
   erb :map
 end
 
