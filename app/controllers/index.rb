de4dfc9ecca550ace6181743012757dabe1d79e5
sollabeck <kevin.ceballos@gmail.com>
2015-03-31 18:34:41 -0700

    send json data from datasf

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,4 @@
-# require 'httparty'
+require 'httparty'
 
 get '/' do
   erb :index
@@ -12,7 +12,7 @@ post '/map' do
   @start_end={starting: params[:start],
    ending: params[:end]
     }.to_json
-  # @data=HTTParty.get('https://data.sfgov.org/resource/w969-5mn4.json?$where=within_circle(latitude,37.78346622,-122.42177834,300)')
+  @data=HTTParty.get('https://data.sfgov.org/resource/w969-5mn4.json?$where=within_circle(latitude,37.78346622,-122.42177834,300)')
   erb :map
 end
 
